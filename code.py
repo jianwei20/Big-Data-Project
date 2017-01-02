7182199d1117b7aeab773dbbf33f4f656fc5bebe
chipong0215 <ponglai22@gmail.com>
2017-01-02 23:41:46 +0800

    Update code.py

--- code.py
+++ code.py
@@ -24,9 +24,9 @@ import time
 start = time.time()
 from pyspark.sql import SQLContext
 from pyspark.sql.types import *
-    sqlContext = SQLContext(sc)
+sqlContext = SQLContext(sc)
 
- customSchema = StructType([
+customSchema = StructType([
       StructField("id", DoubleType(), False),
       StructField("click", DoubleType(), False),
       StructField("hour", StringType(), True),
@@ -52,25 +52,25 @@ from pyspark.sql.types import *
       StructField("C20", DoubleType(), False),
       StructField("C21", DoubleType(), False)])
 # Get file
-    df = sqlContext.read.format("com.databricks.spark.csv").options(header= 'true').schema(customSchema).load("file:///home/bigdatas16/Downloads/train100K.csv")
+df = sqlContext.read.format("com.databricks.spark.csv").options(header= 'true').schema(customSchema).load("file:///home/bigdatas16/Downloads/train100K.csv")
 # Displays the content of the DataFrame to stdout
-    df.show()
+df.show()
 
 from pyspark.ml.feature import StringIndexer
-    data = StringIndexer(inputCol="click", outputCol="label").fit(df).transform(df)
-    data.show()
+data = StringIndexer(inputCol="click", outputCol="label").fit(df).transform(df)
+data.show()
 
 # RFormula
 from pyspark.ml.feature import RFormula
-    formula = RFormula(formula="label ~ banner_pos + app_id + site_category + site_id + site_domain + device_model + C14 + C17 + C18 + C19 + C21 ", featuresCol="features", labelCol="label")
-    output = formula.fit(data).transform(data)
-    data1 = output.select("label", "features")
-    data1.show()
+formula = RFormula(formula="label ~ banner_pos + app_id + site_category + site_id + site_domain + device_model + C14 + C17 + C18 + C19 + C21 ", featuresCol="features", labelCol="label")
+output = formula.fit(data).transform(data)
+data1 = output.select("label", "features")
+data1.show()
 
 # Split training and test data.
     #(training, test) = data1.randomSplit([0.7, 0.3], seed = 12345)
-    training, test = data1.randomSplit([0.7, 0.3], seed = 12345)
-    training.show()
+training, test = data1.randomSplit([0.7, 0.3], seed = 12345)
+training.show()
     
 # Configure an ML pipeline, which consists of three stages: tokenizer, hashingTF, and rf (random forest).
 from pyspark.ml.classification import LogisticRegression
@@ -80,42 +80,41 @@ from pyspark.sql import Row
 from pyspark.ml import Pipeline
 from pyspark.ml.classification import RandomForestClassifier
 from pyspark.ml.feature import StringIndexer, VectorIndexer
-from pyspark.mllib.classification import LogisticRegressionWithLBFGS
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
 from pyspark.mllib.util import MLUtils
-rf = RandomForestClassifier(numTrees = 100, maxDepth = 20, maxBins = 128)
-    pipeline = Pipeline(stages=[rf])
-    pipelineModel = pipeline.fit(training)
-    trainingPredictions = pipelineModel.transform(training)
-    #trainingPredictions.show()
-    trainingPredictions.select("prediction", "label", "features").show()
-    testPredictions = pipelineModel.transform(test)
+rf = RandomForestClassifier(numTrees = 100, maxDepth = 10, maxBins = 128)
+pipeline = Pipeline(stages=[rf])
+pipelineModel = pipeline.fit(training)
+#trainingPredictions = pipelineModel.transform(training)
+#trainingPredictions.show()
+#trainingPredictions.select("prediction", "label", "features").show()
+testPredictions = pipelineModel.transform(test)
 
     #evaluator = MulticlassClassificationEvaluator(
     #labelCol="label", predictionCol="prediction", metricName="precision")
-    evaluator = BinaryClassificationEvaluator()
+evaluator = BinaryClassificationEvaluator()
 from pyspark.mllib.linalg import Vectors
 from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.param import Param, Params
-    evaluatorParaMap = {evaluator.metricName: "areaUnderROC"}
-    aucTraining = evaluator.evaluate(trainingPredictions, evaluatorParaMap)
-    aucTest = evaluator.evaluate(testPredictions, evaluatorParaMap)
+evaluatorParaMap = {evaluator.metricName: "areaUnderROC"}
+#aucTraining = evaluator.evaluate(trainingPredictions, evaluatorParaMap)
+aucTest = evaluator.evaluate(testPredictions, evaluatorParaMap)
     
 # The multiplies out to (2 x 3 x 3) x 10 = 180 different models being trained.
  # k = 3 and k = 10 are common
 from pyspark.ml.tuning import *
-    paramGrid = ParamGridBuilder().addGrid(rf.impurity, ['entropy', 'gini']).addGrid(rf.numTrees, [10, 30, 50]).build()
+paramGrid = ParamGridBuilder().addGrid(rf.impurity, ['entropy', 'gini']).addGrid(rf.numTrees, [10, 30, 50]).build()
  # println(paramGrid(1))
-    cv = CrossValidator().setEstimator(pipeline).setEvaluator(evaluator).setEstimatorParamMaps(paramGrid).setNumFolds(3)
+cv = CrossValidator().setEstimator(pipeline).setEvaluator(evaluator).setEstimatorParamMaps(paramGrid).setNumFolds(3)
  # Run cross-validation, and choose the best set of parameters.
-    cvModel = cv.fit(training)
-    cvPredictions = cvModel.transform(test)
-    cvAUCTest = evaluator.evaluate(cvPredictions, evaluatorParaMap)
+cvModel = cv.fit(training)
+cvPredictions = cvModel.transform(test)
+cvAUCTest = evaluator.evaluate(cvPredictions, evaluatorParaMap)
 
-    cvPredictions.show()
+cvPredictions.show()
 
 #	println("pipeline Training AUC: " + aucTraining)
-    print("pipeline Test AUC: %g" % aucTest)
-    print("Cross-Validation test AUC: %g" % cvAUCTest)
-    end = time.time()
-    print(end - start)
+print("pipeline Test AUC: %g" % aucTest)
+print("Cross-Validation test AUC: %g" % cvAUCTest)
+end = time.time()
+print(end - start)
